661c0155ac9ad779c13dbc0c67d665ccf0bae530
thistonyb <this.tonybaker@gmail.com>
2020-03-27 00:38:40 -0700

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import { movies } from '../data';
+import React from "react";
+import { movies } from "../data";
 
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map(movie => (
+        <div>
+          <h2>Name: {movie.title}</h2>
+          <h3>Time: {movie.time}</h3>
+          <h3>Genres:</h3>
+          <ul>
+            {movie.genres.map(genre => (
+              <li key={genre}>{genre}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
